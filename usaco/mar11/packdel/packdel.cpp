5474df1a72a21389828be2cf7a04b94648580eee
Arkajit Dey <arkajit.dey@gmail.com>
2011-03-14 22:10:55 -0400

    second version of packdel. fixed bug in relax. more comments

--- usaco/mar11/packdel/packdel.cpp
+++ usaco/mar11/packdel/packdel.cpp
@@ -20,27 +20,21 @@ LANG: C++
 
 using namespace std;
 
-template<typename F, typename S>
-struct PairComparator {
-	bool operator() (const pair<F, S>& p1, const pair<F, S>& p2) {
-		return p1.second > p2.second;
-	}
-};
-
 int main() {
 	ofstream fout("packdel.out");
 	ifstream fin("packdel.in");
 
 	typedef pair<int, int> IntPair;
-	typedef struct PairComparator<int, int> IPC;
-//	priority_queue<IntPair, vector<IntPair>, IDPC > q;
-//	vector<IntPair> dists;
 	map<int, vector<IntPair> > graph;
-
 	IntPair edge;
 	vector<IntPair> vec;
+
 	int unvisited[MAXN+1], scores[MAXN+1];
-	int a, b, c, N, M, bestScore;
+	// bit-vector of which nodes we haven't visited
+	// scores is the smallest distance we've found to a particular vertex
+
+	int a, b, c, N, M;
+	int nextVertex; // the vertex with the next best score
 
 	fin >> N >> M;
 
@@ -49,10 +43,7 @@ int main() {
 		graph[i] = vector<IntPair>();
 		unvisited[i] = 1;
 		scores[i] = (i == START) ? 0 : INT_MAX;
-//		dists.push_back(make_pair(i, (i == START) ? 0 : INT_MAX));
-		//q.push(make_pair(i, (i == START) ? 0 : INT_MAX));
 	}
-	bestScore = START;
 
 	for (int i = 1; i <= M; i++) {
 		fin >> a >> b >> c;
@@ -72,23 +63,27 @@ int main() {
 	}
 	*/ 
 
-	/* TEST queue order */
-
+	// We'll visit exactly N vertices
+	// Vist in order of current nextVertex
+	// Find Min currently takes O(N). Can speed up with a min-heap, but didn't
+	// have time to implement.
+	nextVertex = START;
 	for (int i = 1; i <= N; i++) {
-		int v = bestScore;
-		unvisited[v] = 0;
-		vec = graph[v];
+		unvisited[nextVertex] = 0;
+		vec = graph[nextVertex];
 		for (int j = 0; j < vec.size(); j++) {
 			edge = vec[j];
-			if (bestScore + edge.second < scores[edge.first]) {
-				scores[edge.first] = bestScore + edge.second;
+			// Relax operation
+			if (scores[nextVertex] + edge.second < scores[edge.first]) {
+				scores[edge.first] = scores[nextVertex] + edge.second;
 			}
 		}
 
-		bestScore = 0;
+		// find the next unvisited vertex with the lowest score
+		nextVertex = 0;
 		for (int i = 1; i <= N; i++) {
-			if (unvisited[i] && scores[i] < scores[bestScore]) {
-				bestScore = i;
+			if (unvisited[i] && scores[i] < scores[nextVertex]) {
+				nextVertex = i;
 			}
 		}
 	}
