0aca1b901952edd929ba1ad3b9ca6d46605c3cf8
Arkajit Dey <arkajit.dey@gmail.com>
2011-10-30 21:01:00 -0700

    Modularized InteractWith and used it to create a utility for fixing cross-platform line-break inconsistencies.

--- haskell/ch04/InteractWith.hs
+++ haskell/ch04/InteractWith.hs
@@ -1,16 +1,15 @@
 -- file: ch04/InteractWith.hs
 
+module InteractWith where
+-- import only the getArgs function from System.Environment
 import System.Environment (getArgs)
 
 interactWith function inputFile outputFile = do
   input <- readFile inputFile
   writeFile outputFile (function input)
 
-main = mainWith myFunction
-  where mainWith function = do
-          args <- getArgs
-          case args of
-            [input, output] -> interactWith function input output
-            _ -> putStrLn "error: exactly two arguments needed"
-        
-        myFunction = id
+mainWith function = do
+  args <- getArgs
+  case args of
+    [input, output] -> interactWith function input output
+    _ -> putStrLn "error: exactly two arguments needed"
