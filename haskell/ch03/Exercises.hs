47664eb06c91305fab82c2c506eeeeef84a6e179
Arkajit Dey <arkajit@google.com>
2012-01-11 21:16:01 -0800

    Added some haskell exercises.

--- haskell/ch03/Exercises.hs
+++ haskell/ch03/Exercises.hs
@@ -17,5 +17,56 @@ isPalindrome [x] = True
 isPalindrome xs  = ((head xs) == (last xs)) && isPalindrome (tail (init xs))
 
 -- 6. sort list of lists by length
-listCmp xs ys = compare (length xs) (length ys)
-sortLists lists = sortBy listCmp lists
+--listCmp xs ys = compare (length xs) (length ys)
+--sortLists lists = sortBy listCmp lists
+
+isEmpty :: [a] -> Bool
+isEmpty []  = True
+isEmpty _   = False
+
+myHead :: [a] -> a
+myHead []     = error "List empty"
+myHead (x:_)  = x
+
+myTail :: [a] -> [a]
+myTail []     = error "List empty"
+
+-- this matches the : constructor for a list which joins first with rest
+myTail (_:xs) = xs
+
+myLast :: [a] -> a
+myLast []     = error "List empty"
+myLast [x]    = x
+myLast (_:xs) = myLast xs
+
+myInit :: [a] -> [a]
+myInit []       = error "List empty"
+myInit [x]      = []
+--myInit ((xs):x) = xs
+
+-- a can't be any type, it must be an Ord to support < and >= operators
+qsort :: Ord a => [a] -> [a]
+qsort []      = []
+qsort (x:xs)  =   qsort [y | y <- xs, y < x]
+              ++  [x]
+              ++  qsort [y | y <- xs, y >= x]
+
+myMap           :: (a->b) -> [a] -> [b]
+myMap f []      = []
+myMap f (x:xs)  = f x : map f xs
+
+-- infinite lists because of lazy evaluation
+numsFrom n = n : numsFrom (n+1)
+squares = map (^2) (numsFrom 0)  -- ^2 is a "section", a partially applied infix op
+
+-- using boolean guards in pattern matching function definitions
+sign x | x > 0    = 1
+       | x == 0   = 0
+       | x < 0    = -1
+
+-- can also use case expressions
+myTake m ys   = case (m,ys) of
+                  (0,_)     ->  []
+                  (_,[])    ->  []
+                  (n,x:xs)  ->  x : myTake (n-1) xs
+
