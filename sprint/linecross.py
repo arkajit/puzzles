6005d353b500af9370993e61726356d9d4dcf70c
Arkajit Dey <arkajit.dey@gmail.com>
2012-06-17 09:07:16 -0700

    Updated, cleaner solution for linecross.

--- sprint/linecross.py
+++ sprint/linecross.py
@@ -11,60 +11,71 @@ def get_counter(start=1, by=1):
 class Line:
   counter = get_counter()
   def __init__(self, x, y, l):
+    self.id = Line.counter.next()
     self.start = x
     self.end = x+l
     self.pos = y
-    self.id = Line.counter.next()
 
 class Query:
   counter = get_counter()
   def __init__(self, x, y, l):
+    self.id = Query.counter.next()
     self.start = y
     self.end = y+l
     self.pos = x
-    self.id = Query.counter.next()
 
+  def __str__(self):
+    return 'Query(start=%d, end=%d, pos=%d)' % (self.start, self.end, self.pos)
+
+class LineSet:
+  def __init__(self):
+    self.lmap = {}
+
+  def AddLine(self, x, y, l):
+    line = Line(x, y, l)
+    self.lmap[line.id] = line
+
+  # Return set of line ids.
+  def GetLinesBetween(self, y1, y2):
+    lines_by_pos = sorted(self.lmap.itervalues(), key=lambda l: l.pos)
+    lpos = map(lambda l: l.pos, lines_by_pos)
+    a = bisect.bisect_left(lpos, y1)
+    b = bisect.bisect_right(lpos, y2)
+    return set(map(lambda l: l.id, lines_by_pos[a:b]))
+
+  def GetLinesStartingBefore(self, x1):
+    lines_by_start = sorted(self.lmap.itervalues(), key=lambda l: l.start)
+    lstart = map(lambda l: l.start, lines_by_start)
+    a = bisect.bisect_right(lstart, x1)
+    return set(map(lambda l: l.id, lines_by_start[:a]))
+
+  def GetLinesEndingAfter(self, x2):
+    lines_by_end = sorted(self.lmap.itervalues(), key=lambda l: l.end)
+    lend = map(lambda l: l.end, lines_by_end)
+    b = bisect.bisect_left(lend, x2)
+    return set(map(lambda l: l.id, lines_by_end[b:]))
+
+  def exec_query(self, q):
+    a = self.GetLinesBetween(q.start, q.end)
+    b = self.GetLinesStartingBefore(q.pos)
+    c = self.GetLinesEndingAfter(q.pos)
+    return len(set.intersection(a, b, c))
+
+line_set = LineSet()
 N = input()
-lines = []
 for i in range(N):
   x, y, l = [int(i) for i in raw_input().split()]
-  lines.append(Line(x, y, l))
-lines.sort(key=lambda line: line.pos)
+  line_set.AddLine(x, y, l)
 
 Q = input()
 queries = []
 for q in range(Q):
   x, y, l = [int(i) for i in raw_input().split()]
   queries.append(Query(x, y, l))
-queries.sort(key=lambda q: q.pos)
 
-qpos = [q.pos for q in queries]
-matching_queries = []
-for l in lines:
-  # Find queries whose position is between l.start and l.end
-  a = bisect.bisect_left(qpos, l.start)
-  b = bisect.bisect_right(qpos, l.end)
-  matching_queries.append(set(queries[a:b]))
-  #print "Line loop: a = %d, b = %d" % (a, b)
+results = []
+for q in queries:
+  results.append(line_set.exec_query(q))
 
-lpos = [l.pos for l in lines]
-match_count = []
-
-# Loop over queries in original id order.
-for q in sorted(queries, key=lambda q: q.id):
-  cnt = 0
-
-  # Find lines between q.start and q.end
-  a = bisect.bisect_left(lpos, q.start)
-  b = bisect.bisect_right(lpos, q.end)
-  #print "Query loop: a = %d, b = %d" % (a, b)
-  
-  # For each line, check whether query in Dictionary[Line]
-  for i in xrange(a,b):
-    if q in matching_queries[i]:
-      cnt += 1
-
-  match_count.append(cnt)
-
-for mc in match_count:
-  print mc
+for r in results:
+  print r
