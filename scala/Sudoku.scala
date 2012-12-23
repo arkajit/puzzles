aed7290b6040bd8466ec9f96bee09c31f2caca8c
Arkajit Dey <adey@twitter.com>
2012-12-23 13:35:57 -0800

    Add assert checks for C2's units and peers.

--- scala/Sudoku.scala
+++ scala/Sudoku.scala
@@ -25,6 +25,14 @@ object Sudoku extends App {
     assert(unitlist.size == 27)
     assert(squares.forall { units(_).size == 3 })
     assert(squares.forall { peers(_).size == 20 })
+    assert(units("C2") == List(
+      List("A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2", "I2"),
+      List("C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9"),
+      List("A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3")
+    ))
+    assert(peers("C2") == Set("A2", "B2", "D2", "E2", "F2", "G2", "H2", "I2",
+                              "C1", "C3", "C4", "C5", "C6", "C7", "C8", "C9",
+                              "A1", "A3", "B1", "B3"))
     println("All tests pass")
   }
 
