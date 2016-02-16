6155d69d0a7d0bfcc53de006814c5fc28ab5a661
Arkajit Dey <arkajit.dey@gmail.com>
2016-02-15 22:01:12 -0800

    Handle DELETE /items/:id endpoint.

---
+++ javascript/shopping-list/README.md
@@ -0,0 +1,6 @@
+
+== Making sample requests ==
+
+Deleting an item:
+
+curl -H "Content-Type: application/json" -X DELETE http://localhost:8080/items/:id
--- javascript/shopping-list/server.js
+++ javascript/shopping-list/server.js
@@ -1,4 +1,5 @@
-var http = require('http');
+var http   = require('http');
+var _      = require('lodash');
 var static = require('node-static');
 
 var Storage = function() {
@@ -12,6 +13,19 @@ Storage.prototype.add = function(name) {
   this.id += 1;
 };
 
+/**
+ * Removes the item with the given id, if it exists, and returns it.
+ * If the item doesn't exist, returns `undefined` and removes nothing.
+ *
+ */
+Storage.prototype.remove = function(id) {
+  var removed = _.remove(this.items, function(item) {
+    return item.id === id;
+  });
+
+  return removed[0];
+};
+
 var storage = new Storage();
 storage.add('Broad beans');
 storage.add('Tomatoes');
@@ -20,6 +34,8 @@ storage.add('Peppers');
 var fileServer = new static.Server('./public');
 
 var server = http.createServer(function(req, res) {
+  console.log('Received request: ' + req.method + ' ' + req.url);
+
   if (req.method === 'GET' && req.url === '/items') {
     var responseData = JSON.stringify(storage.items);
     res.setHeader('Content-Type', 'application.json');
@@ -43,6 +59,25 @@ var server = http.createServer(function(req, res) {
         res.end(JSON.stringify(responseData));
       }
     });
+  } else if (req.method === 'DELETE' && req.url.indexOf('/items') === 0) {
+    var tokens = req.url.split('/');
+    var itemId = parseInt(tokens[2]);  // maybe NaN
+    if (itemId) {
+      var removedItem = storage.remove(itemId);
+      if (removedItem) {
+        var response = JSON.stringify(removedItem);
+        res.statusCode = 200;
+        res.end(response);
+      } else {
+        var response = {'message': 'Item with id ' + itemId + ' does not exist.'};
+        res.statusCode = 400;
+        res.end(JSON.stringify(response));
+      }
+    } else {
+      var response = {'message': 'Invalid item id.'};
+      res.statusCode = 400;
+      res.end(JSON.stringify(response));
+    }
   } else {
     fileServer.serve(req, res);
   }
